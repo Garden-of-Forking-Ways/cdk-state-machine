fbb1c4422c29f705a47c1bb73e6df92c1130572e
Paul Meier <longish.physic0h@icloud.com>
2023-02-05 16:11:34 -0600

    init

---
+++ cdk_state_machine/cdk_state_machine_stack.py
@@ -0,0 +1,19 @@
+from aws_cdk import (
+    Duration,
+    Stack,
+    RemovalPolicy,
+    aws_sqs as sqs,
+)
+from constructs import Construct
+
+
+class CdkStateMachineStack(Stack):
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        queue = sqs.Queue(
+            self,
+            "CdkStateMachineQueue",
+            visibility_timeout=Duration.seconds(300),
+            props=sqs.QueueProps(removal_policy=RemovalPolicy.DESTROY),
+        )
