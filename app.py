7003d9dab2519f4d7e15e9a399a372395de30566
Paul Meier <paulmeier@northwesternmutual.com>
2023-02-11 18:04:32 -0600

    test

--- app.py
+++ app.py
@@ -6,9 +6,13 @@ import aws_cdk as cdk
 from cdk_state_machine.cdk_state_machine_stack import CdkStateMachineStack
 
 app = cdk.App()
-CdkStateMachineStack(app, "CdkStateMachineStack",
-                     env=cdk.Environment(account=os.getenv('CDK_DEPLOY_ACCOUNT'),
-                                         region=os.getenv('CDK_DEPLOY_REGION')),
-                     )
+CdkStateMachineStack(
+    app,
+    "CdkStateMachineStack",
+    env=cdk.Environment(
+        account=os.getenv('CDK_DEPLOY_ACCOUNT'),
+        region=os.getenv('CDK_DEPLOY_REGION')
+    ),
+)
 
 app.synth()
